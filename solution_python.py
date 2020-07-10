03ea73b35b7347a34a02be2474296d617e5d392f
HarsimranKaur13 <38685587+HarsimranKaur13@users.noreply.github.com>
2020-07-10 12:56:48 -0600

    Add files via upload

--- solution_python.py
+++ solution_python.py
@@ -14,36 +14,36 @@ class EventSourcer():
         if(self.max<self.count+1):
             self.max=self.count+1
         self.count=self.count+1
-        print("value: "+str(self.arr[self.count]))
+        print(str(self.arr[self.count]))
 
     def subtract(self, num: int):
         self.arr.append(self.arr[self.count] - num)
         if(self.max<self.count+1):
             self.max=self.count+1
         self.count=self.count+1
-        print("value: "+str(self.arr[self.count]))
+        print(str(self.arr[self.count]))
 
     def undo(self):
         if(self.count-1>=0):
             self.count=self.count-1
-        print("value: "+str(self.arr[self.count]))
+        print(str(self.arr[self.count]))
 
     def redo(self):
         if(self.count+1<=self.max):
             self.count=self.count+1
-        print("value: "+str(self.arr[self.count]))
+        print(str(self.arr[self.count]))
 
     def bulk_undo(self, steps: int):
         if(self.count-steps>=0):
             self.count=self.count-steps
-            print("value: "+str(self.arr[self.count]))
+            print(str(self.arr[self.count]))
         else:
-            print("value: "+str(self.arr[0]))
+            print(str(self.arr[0]))
 
 
     def bulk_redo(self, steps: int):
         if(self.count+steps<=self.max):
             self.count=self.count+steps
-            print("value: "+str(self.arr[self.count]))
+            print(str(self.arr[self.count]))
         else:
-            print("value: "+str(self.arr[self.max]))
+            print(str(self.arr[self.max]))
